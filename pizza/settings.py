b39d1175b397a2bb3c797024b3a2a5454f9c1a6f
vlad-anisov <anisoffgo@gmail.com>
2020-04-25 12:19:12 +0300

    settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1614@6+1as7i!l4doar2hphyohz3scp4*msuo8^$5q64*l1va4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
