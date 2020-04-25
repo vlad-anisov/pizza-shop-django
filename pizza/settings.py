edd820f7be5a380083019379e7fb4fc763b044d5
vlad-anisov <anisoffgo@gmail.com>
2020-04-25 21:37:24 +0300

    fix

--- pizza/settings.py
+++ pizza/settings.py
@@ -144,13 +144,11 @@ STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
