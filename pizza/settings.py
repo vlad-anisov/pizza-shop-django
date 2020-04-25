ff0634dd3713cd364b9b5766072bdab74c9079d1
vlad-anisov <anisoffgo@gmail.com>
2020-04-25 12:34:24 +0300

    heroku

--- pizza/settings.py
+++ pizza/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pizza-shop-django.herokuapp.com']
 
 
 # Application definition
