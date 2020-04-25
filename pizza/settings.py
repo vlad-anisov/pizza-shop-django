6f02f52fdb47a3fbd03f1bfbbc153698addedcc0
vlad-anisov <anisoffgo@gmail.com>
2020-04-25 18:05:30 +0300

    fix

--- pizza/settings.py
+++ pizza/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
     }
 }
 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
